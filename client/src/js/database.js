7628432902f159df4ec4083ac855ab3db12c766b
Jordan Cardenas <jordancardenas@Jordans-MBP.attlocal.net>
2023-05-23 18:45:01 -0700

    finish todos for databse.js and install.js files

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,44 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+const dbPromise = initdb();
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const putDb = async (content) => {
+  const db = await dbPromise;
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.add({ content });
+  
+  return new Promise((resolve, reject) => {
+    request.onsuccess = () => {
+      console.log('Data added to the database');
+      resolve();
+    };
+
+    request.onerror = () => {
+      console.error('Error adding data to the database');
+      reject(new Error('Failed to add data'));
+    };
+  });
+};
+
+export const getDb = async () => {
+  const db = await dbPromise;
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  
+  return new Promise((resolve, reject) => {
+    request.onsuccess = () => {
+      console.log('Data retrieved from the database');
+      resolve(request.result);
+    };
+
+    request.onerror = () => {
+      console.error('Error retrieving data from the database');
+      reject(new Error('Failed to retrieve data'));
+    };
+  });
+};
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,32 @@
 const butInstall = document.getElementById('buttonInstall');
 
+let deferredPrompt;
+
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
+  deferredPrompt = event;
+  // Show install button or other UI element to prompt the user to install the app
+  butInstall.style.display = 'block';
+});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    const result = await deferredPrompt.userChoice;
+    if (result.outcome === 'accepted') {
+      console.log('App installed');
+    } else {
+      console.log('App installation declined');
+    }
+    deferredPrompt = null;
+    butInstall.style.display = 'none';
+  }
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// Add a handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+  console.log('App installed successfully');
+});
